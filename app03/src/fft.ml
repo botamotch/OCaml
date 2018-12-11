7fbe7ec1a18a5c7c0c3e56923068eba62ee68807
botamotch <laughing00boomer@gmail.com>
2018-12-11 22:46:03 +0900

    modify dtft function (app03)

--- app03/src/fft.ml
+++ app03/src/fft.ml
@@ -3,3 +3,10 @@ open Complex
 let pi = 2.0 *. (acos 0.0)
 let complex_of_float x = { Complex.re = x ; im = 0. }
 
+let rec dtft s omega =
+  let exp_help omega n =
+    Complex.exp (Complex.mul Complex.i (complex_of_float (omega *. (float_of_int n))))
+  in
+  match s with
+  | (hd_int , hd_val) :: tl -> Complex.add (Complex.mul hd_val (exp_help omega hd_int)) (dtft tl omega)
+  | [] -> Complex.zero
--- app03/test.ml
+++ app03/test.ml
@@ -12,16 +12,13 @@ let rec range_float a b step =
   if a >= b then []
   else a :: range_float (a +. step) b step
 
-let dtft s omega =
+let rec dtft s omega =
   let exp_help omega n =
     Complex.exp (Complex.mul Complex.i (complex_of_float (omega *. (float_of_int n))))
   in
-  let rec dtft_help s omega =
-    match s with
-    | (hd_int , hd_val) :: tl -> Complex.add (Complex.mul hd_val (exp_help omega hd_int)) (dtft_help tl omega)
-    | [] -> Complex.zero
-  in
-  dtft_help s omega
+  match s with
+  | (hd_int , hd_val) :: tl -> Complex.add (Complex.mul hd_val (exp_help omega hd_int)) (dtft tl omega)
+  | [] -> Complex.zero
 
 (* Main *)
 let () =
